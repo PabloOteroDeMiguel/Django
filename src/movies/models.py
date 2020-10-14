584e02b8eeed19ecb96e8f77b2ca3e75c51ca9f8
PabloOteroDeMiguel <potero91@gmail.com>
2020-10-14 19:45:42 +0200

    Creamos modelos Category y Movie y sus migraciones

--- src/movies/models.py
+++ src/movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+
+
+class Movie(models.Model):
+
+    title = models.CharField(max_length=150)
+    summary = models.TextField()
+    director_name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    image = models.URLField()
+    rating = models.FloatField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
