81d584fe65543aa2675d301be065771df06a1352
PabloOteroDeMiguel <potero91@gmail.com>
2020-10-15 14:24:33 +0200

    Pintamos las 5 peliculas más actuales

--- src/movies/views.py
+++ src/movies/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from movies.models import Movie
+
 
 def hello_world(request):
     name = request.GET.get("name")
@@ -6,3 +10,8 @@ def hello_world(request):
         return HttpResponse("Hello world")
     else:
         return HttpResponse("Hello " + name)
+
+def home(request):
+    latest_movies = Movie.objects.all().order_by("-release_date")
+    context={'movies': latest_movies[:5]}
+    return render(request, "home.html", context)
