2a5ed24a847a0c8ac4d9bfca148a5e380fde9fa6
PabloOteroDeMiguel <potero91@gmail.com>
2020-10-15 10:18:11 +0200

    Registramos nuestros modelos en el admin de Django

--- src/movies/models.py
+++ src/movies/models.py
@@ -5,6 +5,8 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
 
 class Movie(models.Model):
 
@@ -17,3 +19,6 @@ class Movie(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
