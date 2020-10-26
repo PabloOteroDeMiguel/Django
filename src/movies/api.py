18a4ebebcfe36ea05ca37c3f69a5ce8e74392d97
PabloOteroDeMiguel <potero91@gmail.com>
2020-10-26 16:53:10 +0100

    filtros y buscador

--- src/movies/api.py
+++ src/movies/api.py
@@ -1,3 +1,4 @@
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
@@ -14,6 +15,9 @@ class MoviesListAPI(ListCreateAPIView):
 
     queryset = Movie.objects.all()
     permission_classes = [MoviesPermission]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ["title", "director_name", "category_name"]
+    ordering_fields = ["title", "release_date", "created_at", "modified_at", "director_name", "rating", "category"]
 
     def get_serializer_class(self):
         return MoviesListSerializer if self.request.method == "GET" else MovieSerializer
